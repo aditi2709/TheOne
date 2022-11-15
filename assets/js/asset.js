92a9eedc2ac6f7406106b73b948c58c1cc67b4e5
Aditi Ghosh <aditighosh@Aditis-MacBook-Air.local>
2022-11-15 15:54:35 +0530

    code pushed

--- assets/js/asset.js
+++ assets/js/asset.js
@@ -5,6 +5,32 @@ $('.btn').click(function() {
     $('.modal').modal('show');
 });
 
+
+function getMobileOperatingSystem() {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    console.log(userAgent);
+    if( userAgent.match( /iPad/i ) || userAgent.match( /iPhone/i ) || userAgent.match( /iPod/i ) )
+    {
+      //return 'iOS';
+      $('body').addClass('ios-device');
+      $(".op").append( "iOS" );
+    }
+    else if( userAgent.match( /Android/i ) )
+    {
+      //return 'Android';
+      $('body').addClass('android-device');
+      $(".op").append( "Android" );
+    }
+    else
+    {
+      //return 'unknown'; 
+      $('body').addClass('other-device');
+      $(".op").append( "other" );
+    }
+}
+  
+getMobileOperatingSystem();
+
 // $('.SOS-btn').click(() => {
 //     $('.bottom-sheet-wrapper').addClass('show-modal');
 //   })
