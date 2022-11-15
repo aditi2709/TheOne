2b47a98ff5da58593c6e45f90a7cf22fa49d7251
Aditi Ghosh <aditighosh@Aditis-MacBook-Air.local>
2022-11-15 16:05:33 +0530

    installation link

--- assets/js/asset.js
+++ assets/js/asset.js
@@ -11,21 +11,20 @@ function getMobileOperatingSystem() {
     console.log(userAgent);
     if( userAgent.match( /iPad/i ) || userAgent.match( /iPhone/i ) || userAgent.match( /iPod/i ) )
     {
-      //return 'iOS';
-      $('body').addClass('ios-device');
-      $(".op").append( "iOS" );
+      //return 'iOS';  
+      $(".mobile-install a").attr("href", "https://apps.apple.com/in/app/best-%E0%A4%AA-%E0%A4%B0%E0%A4%B5-%E0%A4%B8-prawas/id1479505664")    
     }
     else if( userAgent.match( /Android/i ) )
     {
       //return 'Android';
-      $('body').addClass('android-device');
-      $(".op").append( "Android" );
+      $(".mobile-install a").attr("href", "https://play.google.com/store/apps/details?id=com.dimts.best.bestapplication&hl=en_IN&gl=US")
     }
     else
     {
+        $(".mobile-install a").attr("href", "https://play.google.com/store/apps/details?id=com.dimts.best.bestapplication&hl=en_IN&gl=US")
       //return 'unknown'; 
-      $('body').addClass('other-device');
-      $(".op").append( "other" );
+    //   $('body').addClass('other-device');
+    //   $(".op").append( "other" );
     }
 }
   
