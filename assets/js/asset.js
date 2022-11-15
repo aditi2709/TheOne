e3f894ff76de084f1ff186fc07b08c3f314ae5ba
Aditi Ghosh <aditighosh@Aditis-MacBook-Air.local>
2022-11-15 17:19:10 +0530

    modal

--- assets/js/asset.js
+++ assets/js/asset.js
@@ -14,21 +14,21 @@ function getMobileOperatingSystem() {
       //return 'iOS';   
       var newUrl = $('.install a').attr('href').replace('#', 'https://apps.apple.com/in/app/best-%E0%A4%AA-%E0%A4%B0%E0%A4%B5-%E0%A4%B8-prawas/id1479505664');
       $('.install a').attr('href', newUrl);
-     //$(".mobile-install a").attr("href", "https://apps.apple.com/in/app/best-%E0%A4%AA-%E0%A4%B0%E0%A4%B5-%E0%A4%B8-prawas/id1479505664")    
+     //$(".install a").attr("href", "https://apps.apple.com/in/app/best-%E0%A4%AA-%E0%A4%B0%E0%A4%B5-%E0%A4%B8-prawas/id1479505664")    
     }
     else if( userAgent.match( /Android/i ) )
     {
       //return 'Android';
       var newUrl = $('.install a').attr('href').replace('#', 'https://play.google.com/store/apps/details?id=com.dimts.best.bestapplication&hl=en_IN&gl=US');
       $('.install a').attr('href', newUrl);
-     // $(".mobile-install a").attr("href", "https://play.google.com/store/apps/details?id=com.dimts.best.bestapplication&hl=en_IN&gl=US")
+     // $(".install a").attr("href", "https://play.google.com/store/apps/details?id=com.dimts.best.bestapplication&hl=en_IN&gl=US")
     }
     else
     {
       //return 'unknown'; 
       var newUrl = $('.install a').attr('href').replace('#', 'https://play.google.com/store/apps/details?id=com.dimts.best.bestapplication&hl=en_IN&gl=US');
       $('.install a').attr('href', newUrl);
-    //   $(".mobile-install a").attr("href", "https://play.google.com/store/apps/details?id=com.dimts.best.bestapplication&hl=en_IN&gl=US")
+    //   $(".install a").attr("href", "https://play.google.com/store/apps/details?id=com.dimts.best.bestapplication&hl=en_IN&gl=US")
     }
 }
   
