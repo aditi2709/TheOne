a11a0e80a0d08b954f084570d5ca5ed5070341f4
Aditi Ghosh <aditighosh@Aditis-MacBook-Air.local>
2022-11-22 15:09:38 +0530

    mqtt

---
+++ config.js
@@ -0,0 +1,19 @@
+// host = '172.16.153.122';	// hostname or IP address
+host = 'mqtt.parxsys.com';	// hostname or IP address
+// host = '172.16.153.110';	// hostname or IP address
+port = 15675;
+topic = 'device/command/Mumbai_Bus_Shelter';		// topic to subscribe to
+useTLS = true;
+username = 'ilovecycling';
+password = 'welcome@234@';
+// username = "jjolie";
+// password = "aa";
+
+// path as in "scheme:[//[user:password@]host[:port]][/]path[?query][#fragment]"
+//    defaults to "/mqtt"
+//    may include query and fragment
+//
+path = "/mqtt";
+// path = "/data/cloud?device=12345";
+
+cleansession = true;
